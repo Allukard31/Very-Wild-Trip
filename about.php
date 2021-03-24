46e1bf54658ef5a74a1651a8b7a94a793257a013
leamahler85 <leamahler85@gmail.com>
2021-03-24 15:58:40 +0100

    Spelling modification in about.php

--- about.php
+++ about.php
@@ -26,7 +26,7 @@ include("header.php");
       <h3 class="profile-name">Lionel G</h3>
       <p class="profile-position">Web Developer</p>
       <p class="profile-body">
-      <scan>Lionel, le <b>sage</b> de notre team Very Wild Trip. Ses plus longues destinations : la Thaïlande, l'île Maurice et la Côte d'Ivoire.<br><br> Bienvenue à bord !</scan>
+      <span>Lionel, le <b>sage</b> de notre team Very Wild Trip. Ses plus longues destinations : la Thaïlande, l'île Maurice et la Côte d'Ivoire.<br><br> Bienvenue à bord !</span>
       </p>
     </div>
   </article>
@@ -39,9 +39,9 @@ include("header.php");
       <h3 class="profile-name">Emmanuel R</h3>
       <p class="profile-position">Web Developer</p>
       <p class="profile-body">
-      <scan><b>"Gamer"</b>invétéré depuis sa tendre enfance.
+      <span><b>"Gamer"</b>invétéré depuis sa tendre enfance.
       "Manu" comme préfère le nommer l'équipe, aime passer son temps (quand il n'est pas en train de jouer...) à écrires des lignes de codes ou faire des blagues à ses collègues de travail.<br>
-      FORZA VIOLA !!!
+      FORZA VIOLA !</span>
       </p>
     </div>
   </article>
@@ -54,7 +54,7 @@ include("header.php");
       <h3 class="profile-name">Quentin B</h3>
       <p class="profile-position">Web Developer</p>
       <p class="profile-body">
-      <scan>Quand il ne joue pas le rôle de <b>"Papa poule"</b>, Quentin voge à travers les lignes de code. La musique agit telle la feuille de route de son périple, tandis que la patience, la créativité et l'humour en sont le carburant !! </scan>
+      <span>Quand il ne joue pas le rôle de <b>"Papa poule"</b>, Quentin vogue à travers les lignes de code. La musique agit telle la feuille de route de son périple, tandis que la patience, la créativité et l'humour en sont le carburant !</span>
       </p>
     </div>
   </article>
